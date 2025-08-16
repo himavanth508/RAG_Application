05cf3cf297d0800cdb41f1ec7be2e140e1f7cdff
himavanth508 <himavanth508@gmail.com>
2025-08-16 20:14:56 +0530

    removed mongodb password

--- backend/db.py
+++ backend/db.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 import os
 
-MONGO_URI = "mongodb+srv://himavanth508:12345@cluster0.643ybf9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"  # Replace with actual URI
+MONGO_URI = "mongodb+srv://himavanth508:<password>@cluster0.643ybf9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"  # Replace with actual URI
 client = MongoClient(MONGO_URI)
 
 db = client["my_rag_app"]  # You can name the DB anything
